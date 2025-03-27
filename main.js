6e75ba340ef987f74f613459aedb363e16e67949
benmertens <ben.mertens@student.uibk.ac.at>
2025-03-27 15:56:14 +0100

    mehrzeiliges Popub mit backticks

--- main.js
+++ main.js
@@ -9,16 +9,19 @@ let zoom = 11;
 //Karte initialisieren
 let map = L.map('map').setView([lat, lng], zoom);
 
-
-
 // Hintergrundkarte definieren
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-        // Marker zeichnen
-        let marker = L.marker([lat, lng]).addTo(map);
+// Marker zeichnen
+let marker = L.marker([lat, lng]).addTo(map);
 
-        //Popup definieren und öffnen
-        marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+//Popup definieren und öffnen
+marker.bindPopup(`
+    <b>Hello world!</b>
+    <br>
+    I am a popup.
+`).openPopup();
+
