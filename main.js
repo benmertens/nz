edfb4da3704391be57ec2d1c4801e96f05a82158
benmertens <ben.mertens@student.uibk.ac.at>
2025-03-27 15:36:12 +0100

    Kommentare ergänzt

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+/*
+Skript für die Neuseelandreise
+*/
+
+//Karte initialisieren
 var map = L.map('map').setView([-40.833333, 172.9], 11);
+
+// Hintergrundkarte definieren
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
+
+        // Marker zeichnen
         var marker = L.marker([-40.833333, 172.9]).addTo(map);
+
+        //Popup definieren und öffnen
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
